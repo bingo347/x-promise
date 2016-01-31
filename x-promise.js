aec9c945c0cdb5ca86abbaab8639af2277dece17
Dmitriy Belyaev <mail@d-belyaev.ru>
2016-01-31 03:06:42 +0300

    add static method props

--- x-promise.js
+++ x-promise.js
@@ -1,8 +1,10 @@
 ;(function(f){typeof Promise==='function'&&f(Promise)}(function(P){
     'use strict';
     var pp = P.prototype;
+    var gopn = Object.getOwnPropertyNames;
 
     function defMethod(target, name, func) {
+        if(typeof target[name] === 'function') return;
         Object.defineProperty(target, name, {
             configurable : true,
             writable : false,
@@ -11,5 +13,20 @@
         });
     }
 
-    
+    defMethod(P, 'props', function props(obj) {
+        var keys = gopn(obj);
+        var i, len = i = keys.length;
+        var arr = new Array(len);
+        while(--i + 1) {
+            arr[i] = obj[keys[i]];
+        }
+        return this.all(arr).then(function(rArr) {
+            var result = {};
+            var i = len;
+            while(--i + 1) {
+                result[keys[i]] = rArr[i];
+            }
+            return result;
+        });
+    });
 }));
